d61d72941eee8e70c856c460e7dfeccb18a15383
Evan Dye <emdye@iastate.edu>
2016-02-27 14:28:51 -0600

    pushing to b

--- hw2/turnin/hw2_dye.java
+++ hw2/turnin/hw2_dye.java
@@ -6,23 +6,43 @@
 public class ChristopherWalkenQuotes {
 	
 	// internal datastructure
-	private Collection<String> quotes = new HashSet<String>();
+	private Collection<Quote> quotes = new ArrayList<Quote>();
 	
-	public ChristopherWalkenQuotes(Collection<String> collection) {
+	public ChristopherWalkenQuotes(Collection<Quote> collection) {
 		quotes.addAll(collection);
 	}
 	
-	public void add(String quote) {
+	public void add(Quote quote) {
 		quotes.add(quote);
+		// TODO enforce uniqueness internally
 	}
 	
 	public boolean remove(Object obj) {
-		return quotes.remove(obj);
+		boolean found = false;
+		while (remove(obj)) {
+			found = true;
+		}
+		return found;
 	}
 	
 	// returns a collection of quotes in ChristopherWalkenQuotes
-	public Collection<String> contained() {
-		return new HashSet<Quote>(quotes);
+	public Collection<Quote> contained() {
+		ArrayList<Quote> retval = new ArrayList<Quote>();
+		
+		for (Quote quote : quotes) {
+			if (!retval.contains(quote)) {
+				retval.add(quote);
+			}
+		}
+		
+		return retval;
+	}
+	
+	public class Quote {
+		public String theQuote;
+		public Quote(String s) {
+			this.theQuote = s;
+		}
 	}
 	
 }
